365d9b0cd124baecb58b83338300423ea08798f9
AnveshaC <anveshachaudhari41@gmail.com>
2025-06-05 21:30:28 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=input("enter the first number:")
+b=input("enter the second number:")
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Additon :",c)
+print("subtraction:",d)
+print("multiplication:",e)
+print("division:",f)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+a=input("Enter your 1st name :")
+b=input("Enter your last name :")
+c=a+b
+print("Hello %s ! welcome to python program "  %c)
